a55d44b16f9dcb6cfcc463a499f8889373bd5805
shubhaan bhandari <shubhaan2004@gmail.com>
2024-08-26 14:58:31 +0530

    Time: 139 ms (53.95%), Space: 78.7 MB (48.41%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,67 +1,25 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
 class Solution {
 public:
-    void merge(vector<int>& nums, int l, int mid, int r) {
-        int n1 = mid - l + 1;
-        int n2 = r - mid;
-
-        // Temporary arrays
-        vector<int> a(n1);
-        vector<int> b(n2);
-
-        // Copy data to temporary arrays
-        for (int i = 0; i < n1; i++) {
-            a[i] = nums[l + i];
-        }
-        for (int i = 0; i < n2; i++) {
-            b[i] = nums[mid + 1 + i];
-        }
-
-        int i = 0; // Initial index of first subarray
-        int j = 0; // Initial index of second subarray
-        int k = l; // Initial index of merged subarray
-
-        // Merge the temp arrays back into nums[l..r]
-        while (i < n1 && j < n2) {
-            if (a[i] <= b[j]) {
-                nums[k] = a[i];
-                i++;
-            } else {
-                nums[k] = b[j];
-                j++;
-            }
-            k++;
-        }
-
-        // Copy the remaining elements of a[], if any
-        while (i < n1) {
-            nums[k] = a[i];
-            i++;
-            k++;
-        }
-
-        // Copy the remaining elements of b[], if any
-        while (j < n2) {
-            nums[k] = b[j];
-            j++;
-            k++;
-        }
-    }
-
-    void mergeSort(vector<int>& nums, int l, int r) {
-        if (l < r) {
-            int mid = l + (r - l) / 2;
-
-            // Sort first and second halves
-            mergeSort(nums, l, mid);
-            mergeSort(nums, mid + 1, r);
-
-            // Merge the sorted halves
-            merge(nums, l, mid, r);
-        }
-    }
-
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
-        return nums;
+        // Create a min-heap
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        // Insert all elements from nums into the min-heap
+        for (int num : nums) {
+            minHeap.push(num);
+        }
+
+        // Extract elements from the min-heap and store them in the result vector
+        vector<int> sortedArray;
+        while (!minHeap.empty()) {
+            sortedArray.push_back(minHeap.top());
+            minHeap.pop();
+        }
+
+        return sortedArray;
     }
 };
