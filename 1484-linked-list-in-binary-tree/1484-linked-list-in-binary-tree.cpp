3c098cdd3628900e1d5713fb6ebccdcfb6babd57
shubhaan bhandari <shubhaan2004@gmail.com>
2024-09-07 23:49:02 +0530

    Time: 23 ms (70.59%), Space: 30.6 MB (95.54%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
@@ -1,21 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool isSubPath(ListNode* head, TreeNode* root) {
-        return dfs(head, head, root);
-    }
-    
-    bool dfs(ListNode* head, ListNode* cur, TreeNode* root) {
-        if (cur == nullptr) return true;  // Successfully matched the list
-        if (root == nullptr) return false; // Reached the end of the tree without matching
-        
-        if (cur->val == root->val) {
-            cur = cur->next;  // Move to the next list node if value matches
-        } else if (head->val == root->val) {
-            head = head->next; // Start new matching attempt if the value matches head of list
-        } else {
-            cur = head;  // Reset the matching pointer
+
+    bool check2(TreeNode* root , ListNode* head){
+        if(head == NULL) return 1;
+        if(root){
+            if(head->val == root->val){
+            return (check2(root->left , head->next) || check2(root->right , head->next));
         }
-        
-        return dfs(head, cur, root->left) || dfs(head, cur, root->right); // Recursively check left and right subtrees
+        }
+
+        return 0;
+    }
+
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if(root == NULL) return 0;
+        if(head->val == root->val){
+            if(check2(root , head)) return 1;
+        }
+
+        return ( isSubPath(head , root->left) || isSubPath(head, root->right) );
+
     }
 };
