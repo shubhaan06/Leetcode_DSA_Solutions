2120d2082035050c7177f9526ee521051cb2d2be
shubhaan bhandari <shubhaan2004@gmail.com>
2024-10-01 22:04:11 +0530

    Time: 38 ms (98.94%), Space: 64.4 MB (76.85%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -4,16 +4,22 @@ public:
         vector<int> freq(k, 0);
         
         for (int num : arr) {
-            int remainder = ((num % k) + k) % k;
-            freq[remainder]++;
+            freq[(num % k + k) % k]++;
         }
-   
-        if (freq[0] % 2 != 0) return false; 
+        
+        if (freq[0] % 2 != 0) return false;
         
         for (int i = 1; i <= k / 2; i++) {
-            if (freq[i] != freq[k - i]) return false; 
+            if (freq[i] != freq[k - i]) return false;
         }
         
         return true;
     }
 };
+static const int KDS = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
+//KDS
